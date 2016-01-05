6cd74441bda4f710e38fe7cae029276a296f16da
Chase Gruber <chasegruber@gmail.com>
2016-01-05 07:37:45 -0500

    change rectangle text to q pts, not cumulative

--- public/js/views/gameView.js
+++ public/js/views/gameView.js
@@ -13,12 +13,12 @@ var GameView = function(game){
 
 GameView.prototype = {
   makeGs:function(svg){
-    var g = svg.selectAll('.game').data(this.game.teams).enter().append('g')
+    var g = svg.selectAll('.game').data(this.game.teams.sort(function(b,a){return b.home - a.home})).enter().append('g')
       .attr('class',function(d){
         return 'game '+d.slug;
       })
       .attr('transform',function(d,i){
-        var y = (d.home)?0:25;
+        var y = (d.home)?25:0;
         return 'translate(0,'+y+')';
       })
     return g;
@@ -60,7 +60,7 @@ GameView.prototype = {
         for (var b=0;b<i+1;b++){
           prevPoint+=vals[b];
         }
-        return "translate("+(self.margins.left+(GameView.scale(prevPoint-1)))+",15)"
+        return "translate("+(self.margins.left+(GameView.scale(prevPoint-0.5)))+",15)"
       })
       .text(function(d,i){
         var prevPoint = 0;
@@ -68,7 +68,8 @@ GameView.prototype = {
         for (var b=0;b<i+1;b++){
           prevPoint+=vals[b]
         }
-        return prevPoint;
+        // return prevPoint;
+        return d;
       })
       .style("font-size",12)
       .style("fill",function(d,i){
@@ -80,31 +81,31 @@ GameView.prototype = {
     var self = this;
     var tops = $("<div/>");
     tops.addClass("tops");
-    tops.html('Top players:');
+    tops.html('Top Performances:');
     teams.forEach(function(team){
       topPlayer = team.topPlayer;
 
       var line = [];
       if(topPlayer.rebounds_total > 3){
-        line.push([topPlayer.rebounds_total,"reb"]);
+        line.push([topPlayer.rebounds_total,"REB"]);
       }
       if(topPlayer.assists > 3){
-        line.push([topPlayer.assists,"ast"]);
+        line.push([topPlayer.assists,"AST"]);
       }
       if(topPlayer.steals > 3){
-        line.push([topPlayer.steals,"stl"]);
+        line.push([topPlayer.steals,"STL"]);
       }
       if(topPlayer.blocks > 3){
-        line.push([topPlayer.blocks,"blk"]);
+        line.push([topPlayer.blocks,"BLK"]);
       }
       line.sort(function(a,b){return b[0] - a[0]});
       var otherStats = [];
       for (var i=0;i<line.length;i++){
-        otherStats.push(line[i].join(""));
+        otherStats.push(line[i].join(" "));
       }
       var otherString = otherStats.join(", ");
-      var lineString = [topPlayer.points+"pts",otherString].join(", ");
-      tops.append("<p>"+team.name+" "+team.nickname+": "+topPlayer.name+" "+lineString+"</p>")
+      var lineString = [topPlayer.points+" PTS",otherString].join(", ");
+      tops.append("<p>"+team.slug.split("-")[1].toUpperCase()+": "+topPlayer.name+" "+lineString+"</p>")
     })
     self.$el.append(tops)
   },
@@ -113,7 +114,7 @@ GameView.prototype = {
     svg.selectAll("g").append("text")
       .style("font-size",12)
       .text(function(d){
-        return d.slug.split("-")[1];
+        return d.slug.split("-")[1].toUpperCase();
       })
       .attr("text-anchor","end")
       .attr("transform",function(d,i){
@@ -122,7 +123,7 @@ GameView.prototype = {
   },
   render:function(){
     this.$el.empty();
-    this.$el.append("<p>"+this.game.matchup+"</p>");
+    this.$el.append("<p class='outcome'>"+this.game.outcome+"</p>");
     var mainW = $(".inner").width();
     var w = (mainW>700)?mainW*0.5:mainW*0.9;
     GameView.scale.range([0,(w-this.margins.right-this.margins.left)])
